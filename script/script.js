859035e05a14a309060ab353fc9196f73321490b
ErdalYildirim <61542138+t7-cmyk@users.noreply.github.com>
2025-09-17 13:13:46 -0400

    Add files via upload

---
+++ script/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+  $(".slider").slick({
+    autoplay: true,
+    autoplaySpeed: 3000,
+    dots: true,
+    arrows: true,
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  });
+});
